0c378d059adc6cd7e12ffc8163f884c2399688de
Paul Stead <paul.stead@zeninternet.co.uk>
2016-05-24 18:14:50 +0100

    Bumped version and added adjustments

--- Concepts.pm
+++ Concepts.pm
@@ -1,20 +1,33 @@
+=head1 NAME
+
+Mail::SpamAssassin::Plugin::Concepts - canonicalise emails into their
+basic concepts. Bayes can use these new tags in future calculations.
+
+=head1 LICENSE
+
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to you under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at:
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+=head1 AUTHOR
+
+Paul Stead <paul.stead@gmail.com>
+
+=cut
+
 package Mail::SpamAssassin::Plugin::Concepts;
-my $VERSION = 0.01;
-
-### Blah:
-#
-# Author: Paul Stead <paul.stead@gmail.com>
-# Copyright 2016 Paul Stead
-#
-# <@LICENSE>
-# TBD
-# </@LICENSE>
-#
-
-### Changelog:
-#
-# 0.01 - first public version
-#
+my $VERSION = 0.02;
 
 use strict;
 use Mail::SpamAssassin::Plugin;
